f7130fe253e6aff53a5e9084e809efa6144889a8
Julio Henrique Moro Balarini <juliohmb@gmail.com>
2022-06-01 11:22:46 -0300

    Sync

---
+++ .config/VSCodium/User/History/693b5b02/j7rX.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Install Yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cd ~/essentials/_Arch
+
+yay -Sy
+# Install Pacman pkgs
+while IFS= read -r line
+do
+  sudo pacman -S --noconfirm --needed $line
+done < "./pacman.txt"
+
+# Install Yay pkgs
+while IFS= read -r line
+do
+  yay -S --needed --noconfirm $line
+done < "./yay.txt"
+
+cargo install exa
